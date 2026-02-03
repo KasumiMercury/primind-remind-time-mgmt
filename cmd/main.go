7f21a24f296084e624f3fdceefb0c6ffd23dd90f
KasumiMercury <naoki_shira@outlook.jp>
2026-02-03 10:16:33 +0900

    fix: add h2c support

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,8 @@ import (
 	"time"
 
 	"connectrpc.com/grpchealth"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/plugin/opentelemetry/tracing"
@@ -157,9 +159,10 @@ func run() error {
 		router.ServeHTTP(w, req)
 	})
 
+	h2s := &http2.Server{}
 	server := &http.Server{
 		Addr:              cfg.Server.Address(),
-		Handler:           httpHandler,
+		Handler:           h2c.NewHandler(httpHandler, h2s),
 		ReadTimeout:       cfg.Server.ReadTimeout,
 		WriteTimeout:      cfg.Server.WriteTimeout,
 		IdleTimeout:       120 * time.Second,
